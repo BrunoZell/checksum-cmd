feb7a04438961902c806ba0e74d9171bc0fd569c
Bruno Zell <bruno.zzell@gmail.com>
2019-01-19 01:38:28 +0100

    Defined cli options

---
+++ src/Checksum/Algorithms.cs
@@ -0,0 +1,13 @@
+﻿namespace Checksum
+{
+    internal enum Algorithms
+    {
+        SHA1,
+        SHA256,
+        SHA384,
+        SHA512,
+        MACTripleDES,
+        MD5,
+        RIPEMD160
+    }
+}
--- src/Checksum/Options.cs
+++ src/Checksum/Options.cs
@@ -1,7 +1,13 @@
-﻿namespace Checksum
+﻿using CommandLine;
+
+namespace Checksum
 {
     internal class Options
     {
+        [Value(0, MetaName = "algorithm", HelpText = "Algorithm to calculate the checksum")]
+        public Algorithms Algorithm { get; set; }
 
+        [Value(1, MetaName = "filename", HelpText = "File name of the file to test")]
+        public string FileName { get; set; }
     }
 }
