43bb01995035e5b272cfe4cf932e79fd39b41ffe
Bruno Zell <bruno.zzell@gmail.com>
2019-01-19 01:29:14 +0100

    Add basic CLI project

---
+++ src/Checksum/Options.cs
@@ -0,0 +1,7 @@
+﻿namespace Checksum
+{
+    internal class Options
+    {
+
+    }
+}
---
+++ src/Checksum/Program.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+using System.Collections.Generic;
+
+namespace Checksum
+{
+    public partial class Program
+    {
+        public static int Main(string[] args) =>
+            Parser.Default.ParseArguments<Options>(args)
+              .MapResult(Main, HandleErrors);
+
+        internal static int Main(Options options) => 0;
+
+        internal static int HandleErrors(IEnumerable<Error> errs) => 1;
+    }
+}
